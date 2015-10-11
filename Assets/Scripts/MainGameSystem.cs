84d9be4a42a190c601b96b1fcfe487d76d3dcc0c
AntoineIturbide <antoine.iturbide@gmail.com>
2015-10-11 20:24:14 +0200

    0.0.2 Edit
    Added [Scripts] folder

--- Assets/Scripts/MainGameSystem.cs
+++ Assets/Scripts/MainGameSystem.cs
@@ -7,4 +7,9 @@ public class MainGameSystem : MonoBehaviour {
 	//List of player contollers
 	List<InputSystem> _playerControllers = new List<InputSystem>();
 
+	//List of characters object
+	List<InputSystem> _playerList = new List<InputSystem>();
+
+
+
 }
